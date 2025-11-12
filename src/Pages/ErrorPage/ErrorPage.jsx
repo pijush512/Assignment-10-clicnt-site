a9c820b95a15cda38955f20ff10e3f05904dff9b
pijush512 <psagorsarker512@gmail.com>
2025-11-12 21:14:12 +0600

    Error page done

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h2>This is Error page</h2>
-    </div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
+      <h1 className="text-6xl font-bold text-purple-500 mb-4">404</h1>
+      <h2 className="text-2xl font-semibold mb-2">Page Not Found</h2>
+      <p className="text-gray-600 mb-6">
+        The page you are looking for does not exist or has been moved.
+      </p>
+      <Link to="/"
+        className="btn btn-primary"
+      >
+        Go to Home
+      </Link>
+    </div >
   );
 };
 
